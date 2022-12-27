1e2065139feb3ea35ad09df3a8b67c1b8f6e689c
Akj1812 <99016901+Akj1812@users.noreply.github.com>
2022-12-27 13:21:18 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,108 @@
+console.log("WELCOME TO PHLINTER-XPRESS");
+alert("THANKS FOR COMING BACK");
+function info(){
+   alert("MY EMAIL ID IS avenzashaft123@gmail.com ");
+}
+function info1(){
+   alert("WELCOME TO THE PHILINTER-XPRESS THIS IS ONLINE MUSIC PLAYER CREATED WITH SOME BEST MELODIUS SONGS ");
+}
+
+function playMyAudio1(){
+
+  document.getElementById("myAudio1").play();
+}
+function pauseMyAudio1(){
+
+  document.getElementById("myAudio1").pause();
+  document.getElementById("audioStatus").innerHTML="Audio Paused";	
+ }
+ function playMyAudio2(){
+    document.getElementById("myAudio2").play();
+ }
+ function pauseMyAudio2(){
+    document.getElementById("myAudio2").pause();
+    document.getElementById("audioStatus").innerHTML="Audio Paused";	
+  }
+  function playMyAudio3(){
+    document.getElementById("myAudio3").play();
+ }
+ function pauseMyAudio3(){
+    document.getElementById("myAudio3").pause();
+    document.getElementById("audioStatus").innerHTML="Audio Paused";	
+ }
+ function playMyAudio4(){
+   document.getElementById("myAudio4").play();
+}
+function pauseMyAudio4(){
+   document.getElementById("myAudio4").pause();
+   document.getElementById("audioStatus").innerHTML="Audio Paused";	
+}
+function playMyAudio5(){
+   document.getElementById("myAudio5").play();
+}
+function pauseMyAudio5(){
+   document.getElementById("myAudio5").pause();
+   document.getElementById("audioStatus").innerHTML="Audio Paused";	
+}
+function playMyAudio6(){
+   document.getElementById("myAudio6").play();
+}
+function pauseMyAudio6(){
+   document.getElementById("myAudio6").pause();
+   document.getElementById("audioStatus").innerHTML="Audio Paused";	
+}
+function playMyAudio7(){
+   document.getElementById("myAudio7").play();
+}
+function pauseMyAudio7(){
+   document.getElementById("myAudio7").pause();
+   document.getElementById("audioStatus").innerHTML="Audio Paused";	
+}
+function playMyAudio8(){
+   document.getElementById("myAudio8").play();
+}
+function pauseMyAudio8(){
+   document.getElementById("myAudio8").pause();
+   document.getElementById("audioStatus").innerHTML="Audio Paused";	
+}
+ function nextAudio1(){
+   
+    document.getElementById("myAudio2").play() &&  document.getElementById("myAudio1").pause();
+
+ }
+
+ function nextAudio2(){
+   
+   document.getElementById("myAudio3").play() &&  document.getElementById("myAudio2").pause();
+
+}
+function nextAudio3(){
+   
+   document.getElementById("myAudio4").play() &&  document.getElementById("myAudio3").pause();
+
+}
+function nextAudio4(){
+   
+   document.getElementById("myAudio5").play() &&  document.getElementById("myAudio4").pause();
+
+}
+function nextAudio5(){
+   
+   document.getElementById("myAudio6").play() &&  document.getElementById("myAudio5").pause();
+
+}
+function nextAudio6(){
+   
+   document.getElementById("myAudio7").play() &&  document.getElementById("myAudio6").pause();
+
+}
+function nextAudio7(){
+   
+   document.getElementById("myAudio8").play() &&  document.getElementById("myAudio7").pause();
+
+}
+function nextAudio8(){
+   
+   document.getElementById("myAudio9").play() &&  document.getElementById("myAudio8").pause();
+
+}
